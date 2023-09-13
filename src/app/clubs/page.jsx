6c0df0815dd38ff60c371ca88454bdb30116659e
lvizzolini <lvizzolini@basecodeit.com>
2023-09-12 21:54:54 -0300

    first commit with next environment

---
+++ src/app/clubs/page.jsx
@@ -0,0 +1,3 @@
+export default function ClubsPage() {
+  return <div>Esta es la pagina de un club</div>;
+}
---
+++ src/app/league/page.jsx
@@ -0,0 +1,3 @@
+export default function LeaguePage() {
+  return <div>Esta es la pagina de la liga</div>;
+}
